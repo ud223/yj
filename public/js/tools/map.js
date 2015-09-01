e4003abb643e67b9208043b5184aa30f31f8be16
ud223 <108234154@qq.com>
2015-09-01 15:33:34 +0800

    1

--- public/js/tools/map.js
+++ public/js/tools/map.js
@@ -23,7 +23,7 @@ function getAddressByCoordinate(lng, lat, fun) {
 }
 
 function getAddressByAddressName(address_name, fun) {
-   var url = "http://api.map.baidu.com/geocoder/v2/?ak=8TN0gC5Rqo6cec2jroKOkNpE&callback=renderOption&output=json&address="+ address_name +"&city=深圳市";
+   var url = "http://api.map.baidu.com/geocoder/v2/?ak=8TN0gC5Rqo6cec2jroKOkNpE&callback=renderOption&output=json&address="+ address_name +"&city=武汉市";
 
     $.ajax({
         url: url,

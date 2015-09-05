d5b1ca637f301f3edab5aaf1e69dd5fefafaa18a
ud223 <108234154@qq.com>
2015-09-06 02:41:57 +0800

    1

--- public/js/controller/index/index.js
+++ public/js/controller/index/index.js
@@ -33,13 +33,8 @@ function initUser() {
     var regions = $(".tcfilter-filter").find(".itmbtn");
 
     $(regions[1]).addClass("selected");//服务器用:55e307d17406f725038b4679
-<<<<<<< HEAD
 //测试用:55caf092d53de42d048b456c//55ced8b3d53de429048b456b
-    localStorage.setItem('user_id', '55ced8b3d53de429048b456b');
-=======
-//测试用:55caf092d53de42d048b456c   55e307d17406f725038b4679
-    localStorage.setItem('user_id', '55caf092d53de42d048b456c');
->>>>>>> 7e09eae9e79efaf890e7eb2f51f50adc17dcbb15
+    localStorage.setItem('user_id', '55e307d17406f725038b4679');
 
     var user_id = localStorage.getItem('user_id')
 

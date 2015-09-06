4c34a8d6a802db021e8af744d956e2b0e8e54350
ud223 <108234154@qq.com>
2015-09-06 16:34:24 +0800

    1

--- application/modules/angel/controllers/IndexController.php
+++ application/modules/angel/controllers/IndexController.php
@@ -182,19 +182,25 @@ class Angel_IndexController extends Angel_Controller_Action {
     }
 
     public function  regAction() {
-        exit("xixi");
+//        exit("xixi");
         $code = $_GET['code'];
         $id = $this->getParam('id');
 
         $tmp_web_url = $this->getParam('web_url');
         $web_url = urldecode($tmp_web_url);
 //        exit($web_url);
-        if ($web_url == "http://www.yujiaqu.com/") {
-            $web_url = "/";
+        if ($web_url) {
+            if ($web_url == "http://www.yujiaqu.com/") {
+                $web_url = "/";
+            }
+            else {
+                $web_url = str_replace("http://www.yujiaqu.com/", "/", $web_url);
+            }
         }
         else {
-            $web_url = str_replace("http://www.yujiaqu.com/", "/", $web_url);
+            $web_url = "/";
         }
+
 //        exit($web_url);
         $open_id = $this->getOpenId($code);
         $userInfo = $this->getUserInfo($open_id);
--- public/js/tools/wx.js
+++ public/js/tools/wx.js
@@ -13,8 +13,9 @@ function validEnvironment() {
 function userLogin() {
     var user_id = localStorage.getItem('user_id');
     //var url = encodeURIComponent("http://www.yujiaqu.com/reg");
+    var tmp_url = location.href;
 
-    var url = encodeURI("http://www.yujiaqu.com/reg");
+    var url = encodeURI("http://www.yujiaqu.com/reg?web_url=" + tmp_url);
 
     if (!user_id) {
         var toUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+ appid +"&redirect_uri="+ url +"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";

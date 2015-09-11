0fc96866eb93aa97cdb049d64f0da20fcfee6967
ud223 <108234154@qq.com>
2015-09-11 15:42:23 +0800

    1

--- public/js/controller/index/index.js
+++ public/js/controller/index/index.js
@@ -36,7 +36,7 @@ function initUser() {
 
     $(regions[1]).addClass("selected");//服务器用:55e307d17406f725038b4679
 //测试用:55caf092d53de42d048b456c//55ced8b3d53de429048b456b
-//    localStorage.setItem('user_id', '55caf092d53de42d048b456c');
+    localStorage.setItem('user_id', '55ced8b3d53de429048b456b');
 
     //var user_id = localStorage.getItem('user_id')
     //

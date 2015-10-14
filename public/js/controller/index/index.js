804397c710aa04fbfb12bb1748182d7668bd239c
ud223 <108234154@qq.com>
2015-10-14 21:24:09 +0800

    1

--- public/js/controller/index/index.js
+++ public/js/controller/index/index.js
@@ -40,7 +40,12 @@ function initUser() {
     //var regions = $(".tcfilter-filter").find(".itmbtn");
     //
     //$(regions[1]).addClass("selected");
+    //55f3d0607406f74f758b4624 彭燕
+    //55f29c427406f750758b45db	彭彩红
+    //56049fad7406f7536f8b45fb 傅丽君
+    //5604a6677406f7007f8b4568	温利华
+    //55f682737406f74f758b4646	刘郎平
 //服务器测试：55ebe5c67406f7ba678b4573/55f3d0607406f74f758b4624
 //测试用:55caf092d53de42d048b456c//55ced8b3d53de429048b456b//55f2d8bfd53de415048b4569
-    localStorage.setItem('user_id', '55ebe5c67406f7ba678b4573');
+    localStorage.setItem('user_id', '55f3d0607406f74f758b4624');
 }

c7ef22334ab2f5170aa36c1859c8aa258b341f3b
ud223 <108234154@qq.com>
2015-10-14 17:22:40 +0800

    1

--- public/js/controller/index/index.js
+++ public/js/controller/index/index.js
@@ -42,5 +42,5 @@ function initUser() {
     //$(regions[1]).addClass("selected");
 //服务器测试：55ebe5c67406f7ba678b4573
 //测试用:55caf092d53de42d048b456c//55ced8b3d53de429048b456b//55f2d8bfd53de415048b4569
-//    localStorage.setItem('user_id', '55ebe5c67406f7ba678b4573');
+    localStorage.setItem('user_id', '55ebe5c67406f7ba678b4573');
 }

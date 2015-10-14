d8fa3f49e6a733d67467345144bf8764e3596085
ud223 <108234154@qq.com>
2015-10-14 21:31:39 +0800

    1

--- public/js/controller/index/index.js
+++ public/js/controller/index/index.js
@@ -47,5 +47,5 @@ function initUser() {
     //55f682737406f74f758b4646	刘郎平
 //服务器测试：55ebe5c67406f7ba678b4573/55f3d0607406f74f758b4624
 //测试用:55caf092d53de42d048b456c//55ced8b3d53de429048b456b//55f2d8bfd53de415048b4569
-    localStorage.setItem('user_id', '56049fad7406f7536f8b45fb');
+    localStorage.setItem('user_id', '5604a6677406f7007f8b4568');
 }

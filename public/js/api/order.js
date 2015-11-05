9ae2209a18725d7d84b0a5bbb860865121656ba8
ud223 <108234154@qq.com>
2015-11-05 19:28:52 +0800

    1

--- public/js/api/order.js
+++ public/js/api/order.js
@@ -30,7 +30,7 @@ function confirmOrder(rundate, time, hour, amount, pay_amount, address, address_
 
     var  data = { 'id':order_id, 'pay_amount':pay_amount, 'rundate':rundate, 'time':time, 'hour':hour, 'price': order_price, 'amount':amount, 'customer_name':customer_name, 'address':address, 'address_detail':address_detail, 'phone':phone, 'lat':lat, 'lng':lng, 'coupon_id': coupon_id }
     alert(JSON.stringify(data));
-    return;
+    //return;
     $.ajax({
         url: url,
         dataType: 'json',
